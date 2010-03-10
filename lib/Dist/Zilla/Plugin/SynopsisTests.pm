69513575bc90e087c86057579205109fe10d1556
Marcel Gruenauer <hanekomu@gmail.com>
2010-03-11 00:13:15 +0100

    initial commit

---
+++ lib/Dist/Zilla/Plugin/SynopsisTests.pm
@@ -0,0 +1,45 @@
+use 5.008;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::SynopsisTests;
+# ABSTRACT: release tests for synopses
+use Moose;
+use Test::Synopsis;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=pod
+
+=for test_synopsis
+1;
+__END__
+
+=head1 SYNOPSIS
+
+In C<dist.ini>:
+
+    [SynopsisTests]
+
+=head1 DESCRIPTION
+
+This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
+following files
+
+  xt/release/synopsis.t - a standard Test::Synopsis test
+
+=cut
+
+__DATA__
+___[ xt/release/synopsis.t ]___
+#!perl
+
+use Test::More;
+
+eval "use Test::Synopsis";
+plan skip_all => "Test::Synopsis required for testing synopses"
+  if $@;
+all_synopsis_ok('lib');
